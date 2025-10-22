f13802fff347c971826651b72c9faee3e730979a
Saravanabaluns <saravana@balunstech.com>
2025-10-22 10:07:40 +0530

    Modified for send enquiry

--- baluns/send_email.php
+++ baluns/send_email.php
@@ -1,14 +1,14 @@
 <?php
-
-// Get inputs from POST
-$address   = $_POST['address'];
-$location  = $_POST['location'];
-$mapsLink  = $_POST['mapsLink'];
-$clickType = $_POST['clickType']; // Get the slide title
+// Sanitize POST inputs
+$address   = htmlspecialchars(strip_tags($_POST['address']));
+$location  = htmlspecialchars(strip_tags($_POST['location']));
+$mapsLink  = filter_var($_POST['mapsLink'], FILTER_SANITIZE_URL);
+$clickType = htmlspecialchars(strip_tags($_POST['clickType']));
 
 // Prepare mail
-$to = "amutha@balunstech.com, sales@balunstech.com";
+$to = "amutha@balunstech.com,sales@balunstech.com";
 $subject = "New Location Submission";
+
 $message = '<html><body>';
 $message .= '<h2>User Location Details:</h2>';
 $message .= '<p>
@@ -19,14 +19,17 @@ $message .= '<p>
              </p>';
 $message .= '</body></html>';
 
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers .= "From: <sales@balunstech.com>" . "\r\n";
+$headers  = "MIME-Version: 1.0\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8\r\n";
+$headers .= "From: sales@balunstech.com\r\n";
+$headers .= "Reply-To: sales@balunstech.com\r\n";
+$headers .= "Return-Path: sales@balunstech.com\r\n";
 
 $mail = mail($to, $subject, $message, $headers);
 
 if ($mail) {
     echo "Mail sent successfully";
 } else {
-    echo "Mail sending failed";
+    echo "Mail sending failed: " . error_get_last()['message'];
 }
+?>
